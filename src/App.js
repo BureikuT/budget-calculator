527bf6dba26aff1b5d36ec28e4dde5bec0dd88e2
BureikuT <bureikut@hotmail.com>
2020-01-23 13:41:39 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+import ExpenseList from './components/ExpenseList'
+import ExpenseItem from './components/ExpenseItem'
+import ExpenseForm from './components/ExpenseForm'
+import Alert from './components/Alert'
+export const App = () => {
+  return <>
+  <Alert></Alert>
+ < ExpenseForm/>
+ <ExpenseList/>
+  )
+  </>
 }
 
-export default App;
+export default App
---
+++ src/components/ExpenseList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Item from './ExpenseItem'
+
+export const ExpenseList = () => {
+    return (
+        <div>
+            <Item/>
+        </div>
+    )
+}
+
+export default ExpenseList
