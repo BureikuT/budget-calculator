14717556c116d12e76a25c47041d209109a6eb86
BureikuT <bureikut@hotmail.com>
2020-01-23 14:02:45 -0700

    added dummy data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ExpenseList from './components/ExpenseList'
 import ExpenseItem from './components/ExpenseItem'
 import ExpenseForm from './components/ExpenseForm'
 import Alert from './components/Alert'
-export const App = () => {
+import uuid from 'uuid/v4'
+
+const initialExpenses = [
+  {id:uuid(), charge: "rent", amount: 1000},
+  {id:uuid(), charge: "car payment", amount: 2000},
+  {id:uuid(), charge: "credit card", amount: 3000}
+]
+
+console.log(initialExpenses)
+
+
+const App = () => {
   return <>
-  <Alert></Alert>
+  <Alert/>
  < ExpenseForm/>
  <ExpenseList/>
-  )
+  
   </>
 }
 
