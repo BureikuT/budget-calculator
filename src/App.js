8765cf8f2d96c2d8f06ca4c5f04652ee8dc7bf07
BureikuT <bureikut@hotmail.com>
2020-01-23 14:15:10 -0700

    have a reduce function to add up the ammounts in expense array

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React, {useState} from 'react'
-import ExpenseList from './components/ExpenseList'
-import ExpenseItem from './components/ExpenseItem'
-import ExpenseForm from './components/ExpenseForm'
-import Alert from './components/Alert'
-import uuid from 'uuid/v4'
+import React, { useState } from "react";
+import ExpenseList from "./components/ExpenseList";
+import ExpenseItem from "./components/ExpenseItem";
+import ExpenseForm from "./components/ExpenseForm";
+import Alert from "./components/Alert";
+import uuid from "uuid/v4";
 
 const initialExpenses = [
-  {id:uuid(), charge: "rent", amount: 1000},
-  {id:uuid(), charge: "car payment", amount: 2000},
-  {id:uuid(), charge: "credit card", amount: 3000}
-]
+  { id: uuid(), charge: "rent", amount: 1000 },
+  { id: uuid(), charge: "car payment", amount: 2000 },
+  { id: uuid(), charge: "credit card", amount: 3000 }
+];
 
-console.log(initialExpenses)
+function App() {
+  const [expenses, setExpenses] = useState(initialExpenses);
 
-
-const App = () => {
-  return <>
-  <Alert/>
- < ExpenseForm/>
- <ExpenseList/>
-  
-  </>
+  return (
+    <>
+      <Alert />
+      <h1>budget calculator</h1>
+      <main className="App">
+        <ExpenseForm />
+        <ExpenseList expenses={expenses} />
+      </main>
+      <h1>
+        total spending :{" "}
+        <span className="total">
+          $
+          {expenses.reduce((acc, curr) => {
+            return (acc += curr.amount);
+          }, 0)}
+        </span>
+      </h1>
+    </>
+  );
 }
 
-export default App
+export default App;
