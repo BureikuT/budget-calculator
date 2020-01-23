2d9feb434fa2491b7986a3e636a21605ebfbebcb
BureikuT <bureikut@hotmail.com>
2020-01-23 16:00:33 -0700

    can now add a single expense to the dummy data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import './App.css'
+import "./App.css";
 import ExpenseList from "./components/ExpenseList";
 import ExpenseItem from "./components/ExpenseItem";
 import ExpenseForm from "./components/ExpenseForm";
@@ -14,13 +14,38 @@ const initialExpenses = [
 
 function App() {
   const [expenses, setExpenses] = useState(initialExpenses);
+  const [charge, setCharge] = useState("");
+  const [amount, setAmount] = useState("");
 
+  const handleCharge = e => {
+    setCharge(e.target.value);
+  };
+
+  const handleAmount = e => {
+    setAmount(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(charge !== '' && amount  > 0){
+const singleExpense = {id: uuid(), charge, amount}
+setExpenses([...expenses, singleExpense])
+    }else{
+console.log('error')
+    }
+  };
   return (
     <>
       <Alert />
       <h1>budget calculator</h1>
       <main className="App">
-        <ExpenseForm />
+        <ExpenseForm
+          charge={charge}
+          amount={amount}
+          handleCharge={handleCharge}
+          handleAmount={handleAmount}
+          handleSubmit={handleSubmit}
+        />
         <ExpenseList expenses={expenses} />
       </main>
       <h1>
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,24 @@
-import React from 'react'
+import React from "react";
+import { MdEdit, MdDelete } from "react-icons/md";
 
-export const ExpenseItem = ({expense}) => {
-    const{id,charge,amount} = expense
+export const ExpenseItem = ({ expense }) => {
+  const { id, charge, amount } = expense;
 
-    return (
-        <li className='item'>
-        <div className='info'>
-            <span className='expense'>{charge}</span>
-            <span></span>
-        </div>
-        </li>
-    )
-}
+  return (
+    <li className="item">
+      <div className="info">
+        <span className="expense">{charge}</span>
+        <span className="amount">${amount}</span>
+      </div>
+      <div>
+        {" "}
+        <button className="edit-btn" area-label="edit button">
+          <MdEdit />
+        </button>
+      </div>
+      <div> <button className='clear-btn' area-label='delete button'><MdDelete/></button></div>
+    </li>
+  );
+};
 
-
-export default ExpenseItem
+export default ExpenseItem;
